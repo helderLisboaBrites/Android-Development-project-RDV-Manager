eada80d5df181636c260e1a05b1749c62084760e
Helder Lisboa Brites <58085537+helderLisboaBrites@users.noreply.github.com>
2021-03-25 23:54:21 +0100

    interface rdv details+ main activity + debut config ET

--- app/src/main/java/com/example/android_development_project_rdv_manager/MainActivity.java
+++ app/src/main/java/com/example/android_development_project_rdv_manager/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.android_development_project_rdv_manager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.rdv_manager_menu,menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_celebrity:{
+                Intent vIntent = new Intent(this, RdvManagerDetailsActivity.class );
+                vIntent.putExtra("fromAdd",true);
+                startActivity(vIntent);
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
